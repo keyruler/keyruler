94c889d4b9e2969c20923df4926a59b81dc0ac24
Henrik Olsson <saviours-kid97@hotmail.se>
2019-01-16 16:52:08 +0100

    Switch to kid

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ const crypto = require('crypto');
 const port = process.env.PORT || 3004;
 
 const db = new SQLite.Database('keyruler');
-db.run("CREATE TABLE IF NOT EXISTS 'keys' ('keyId' TEXT NOT NULL, 'key' TEXT NOT NULL, 'context' TEXT NOT NULL, PRIMARY KEY ('keyId'))");
+db.run("CREATE TABLE IF NOT EXISTS 'keys' ('kid' TEXT NOT NULL, 'key' TEXT NOT NULL, 'context' TEXT NOT NULL, PRIMARY KEY ('kid'))");
 
 fastify.post('/newKey', (req, reply) => {
-    const keyId = crypto.randomBytes(5).toString('base64');
+    const kid = crypto.randomBytes(5).toString('base64');
 
     // TODO: Change to unique and more secure key?
     // TODO: Use Shamirs Secret Sharing for multi keys?
     const key = crypto.randomBytes(32).toString('base64');
 
-    db.run("INSERT INTO 'keys' (keyId, key, context) VALUES (?, ?, ?)", [keyId, key, req.query.context], (err) => {
+    db.run("INSERT INTO 'keys' (kid, key, context) VALUES (?, ?, ?)", [kid, key, req.query.context], (err) => {
         if (!err) {
-            reply.send({ keyId, key });
+            reply.send({ kid, key });
         } else {
             reply.code(500).send();
         }
@@ -25,7 +25,7 @@ fastify.post('/newKey', (req, reply) => {
 
 
 fastify.get('/getKey', (req, reply) => {
-    db.get("SELECT * FROM 'keys' WHERE (keyId=?)", req.query.keyId, (err, row) => {
+    db.get("SELECT * FROM 'keys' WHERE (kid=?)", req.query.kid, (err, row) => {
         if (!err && row !== undefined) {
             reply.send({ key: row.key });
         } else {
@@ -35,7 +35,7 @@ fastify.get('/getKey', (req, reply) => {
 });
 
 fastify.delete('/deleteKey', (req, reply) => {
-    db.run("DELETE * FROM 'keys' WHERE (keyId=?)", req.query.keyId, (err) => {
+    db.run("DELETE * FROM 'keys' WHERE (kid=?)", req.query.kid, (err) => {
         if (!err && this.changes) {
             reply.code(200).send();
         } else {
